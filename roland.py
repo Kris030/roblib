936d65222ac729205ebc87f8c84566efb3233b4c
danipatko <p8.pada@gmail.com>
2021-08-05 08:21:05 +0200

    update roland.py

--- roland.py
+++ roland.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #-*- coding:UTF-8 -*-
 #mit hozzunk a táborba?
 
@@ -6,7 +5,7 @@
 #2 mérőszalag
 #3 csavarhúzó, bitkészlet
 #4 pótcsavarok
-
+import wiringpi
 import RPi.GPIO as GPIO # Raspberry Pi General Purpose Input/Output
 print("Roland betöltve.")
 import time # delayekhez kell
@@ -19,24 +18,12 @@ IN3 = 19 # jobb előre
 IN4 = 26 # jobb hátra
 ENA = 16 # bal sebesség (pwm)
 ENB = 13 # jobb sebesség (pwm)
-=======
-import RPi.GPIO as GPIO
-
-# Motors
-IN1 = 20 # left forward
-IN2 = 21 # left backward
-IN3 = 19 # right forward
-IN4 = 26 # right backward
-ENA = 16 # left speed (pwm)
-ENB = 13 # right speed (pwm)
->>>>>>> f9b84043af9235dec571e376bcb1a1aff3f921da
 
 # RGB module pin
 LED_R = 22
 LED_G = 27
 LED_B = 24
 
-<<<<<<< HEAD
 #Definition of  button
 key = 8
 
@@ -81,6 +68,11 @@ GPIO.setup(TrackSensorLeftPin2,GPIO.IN)
 GPIO.setup(TrackSensorRightPin1,GPIO.IN)
 GPIO.setup(TrackSensorRightPin2,GPIO.IN)
 
+# buzzer setup
+buzzer_pin = 10
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(buzzer_pin, 1)
+wiringpi.softPwmCreate(buzzer_pin, 0, 100)
 
 def motor(vb,vj):
 #vb és vj rendre a bal és jobb oldali sebesség 1 és 100 közötti értéke
@@ -110,69 +102,14 @@ def countdown(v):
         time.sleep(1)
         
 def alszik ():
-=======
-# TrackSensorLeftPin1 TrackSensorLeftPin2 TrackSensorRightPin1 TrackSensorRightPin2
-#      	3                 5                  4                   18
-TrackSensorLeftPin1  =  3    # The first tracking infrared sensor pin on the left is connected to  BCM port 3 of Raspberry pi
-TrackSensorLeftPin2  =  5    # The second tracking infrared sensor pin on the left is connected to  BCM port 5 of Raspberry pi
-TrackSensorRightPin1 =  4    # The first tracking infrared sensor pin on the right is connected to  BCM port 4 of Raspberry pi
-TrackSensorRightPin2 =  18   # The second tracking infrared sensor pin on the right is connected to  BCMport 18 of Raspberry pi
-
-def init():
-
-	#Set the GPIO port to BCM encoding mode.
-	GPIO.setmode(GPIO.BCM)
-
-	# LED init
-	GPIO.setup(LED_R, GPIO.OUT)
-	GPIO.setup(LED_G, GPIO.OUT)
-	GPIO.setup(LED_B, GPIO.OUT)
-
-	# Ignore warning information
-	GPIO.setwarnings(False)
-
-	# Motor setup
-	GPIO.setup(ENA, GPIO.OUT, initial = GPIO.HIGH)
-	GPIO.setup(IN1, GPIO.OUT, initial = GPIO.LOW)
-	GPIO.setup(IN2, GPIO.OUT, initial = GPIO.LOW)
-	GPIO.setup(ENB, GPIO.OUT, initial = GPIO.HIGH)
-	GPIO.setup(IN3, GPIO.OUT, initial = GPIO.LOW)
-	GPIO.setup(IN4, GPIO.OUT, initial = GPIO.LOW)
-
-	global pwm_ENA, pwm_ENB
-	pwm_ENA = GPIO.PWM(ENA, 2000) # pulse width modulation: 16-os pin 2000Hz-cel
-	pwm_ENB = GPIO.PWM(ENB, 2000)
-
-	# Track sensor setup
-	GPIO.setup(TrackSensorLeftPin1,		GPIO.IN)
-	GPIO.setup(TrackSensorLeftPin2,		GPIO.IN)
-	GPIO.setup(TrackSensorRightPin1,	GPIO.IN)
-	GPIO.setup(TrackSensorRightPin2,	GPIO.IN)
-	
-	print("Robot loaded.")
-
-def motor(left, right):
-    pwm_ENA.start(abs(left))
-    pwm_ENB.start(abs(right))
-    GPIO.output(IN1, GPIO.HIGH if left > 0 else GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW if left >= 0 else GPIO.HIGH)
-    GPIO.output(IN3, GPIO.HIGH if right > 0 else GPIO.LOW)
-    GPIO.output(IN4, GPIO.LOW if right >= 0 else GPIO.HIGH)
-
-def led(r, g, b):
-    GPIO.output(LED_R, GPIO.HIGH if r else GPIO.LOW)
-    GPIO.output(LED_G, GPIO.HIGH if g else GPIO.LOW)
-    GPIO.output(LED_B, GPIO.HIGH if b else GPIO.LOW)
-
-def tracksensor():
-    ts1 = GPIO.input(TrackSensorLeftPin1)
-    ts2 = GPIO.input(TrackSensorLeftPin2)
-    ts3 = GPIO.input(TrackSensorRightPin1)
-    ts4 = GPIO.input(TrackSensorRightPin2)
-    return (ts1, ts2, ts3, ts4)
-
-def sleep():
->>>>>>> f9b84043af9235dec571e376bcb1a1aff3f921da
     pwm_ENA.stop()
     pwm_ENB.stop()
     GPIO.cleanup()
+
+def buzzer(pw, ms):
+    wiringpi.softPwmWrite(buzzer_pin, pw)
+    wiringpi.delay(ms)
+    wiringpi.softPwmWrite(buzzer_pin, 100)
+
+
+
